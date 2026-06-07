2ff81a32b1434046ab168ead46123f9685f1a475
Ruchira Ravindra Karle <154701674+ruchirakarle@users.noreply.github.com>
2026-06-07 18:11:27 -0400

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -107,3 +107,23 @@ resource "aws_vpc_endpoint" "s3" {
   route_table_ids = [aws_route_table.private.id]
   tags = { Name = "securescan-s3-endpoint" }
 }
+
+output "vpc_id" {
+  value = aws_vpc.securescan.id
+}
+
+output "public_subnet_1_id" {
+  value = aws_subnet.public_1.id
+}
+
+output "public_subnet_2_id" {
+  value = aws_subnet.public_2.id
+}
+
+output "private_subnet_1_id" {
+  value = aws_subnet.private_1.id
+}
+
+output "private_subnet_2_id" {
+  value = aws_subnet.private_2.id
+}
