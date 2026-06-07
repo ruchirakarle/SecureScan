522c72850ea95fafce6147b1fcf33a1e252cecae
Ruchira Ravindra Karle <154701674+ruchirakarle@users.noreply.github.com>
2026-06-07 18:13:42 -0400

    Create variables.tf

---
+++ terraform/modules/cloudwatch/variables.tf
@@ -0,0 +1,27 @@
+variable "sns_topic_arn" {
+  type        = string
+  description = "SNS topic ARN for alerts"
+}
+
+variable "ec2_instance_id" {
+  type        = string
+  description = "EC2 instance ID"
+}
+
+variable "ecs_cluster_name" {
+  type        = string
+  description = "ECS cluster name"
+  default     = "securescan-cluster"
+}
+
+variable "ecs_service_name" {
+  type        = string
+  description = "ECS pentest service name"
+  default     = "securescan-pentest-service"
+}
+
+variable "dlq_name" {
+  type        = string
+  description = "Dead letter queue name"
+  default     = "securescan-pentest-dlq"
+}
