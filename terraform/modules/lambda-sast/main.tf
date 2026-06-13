774b82da27b0bd1cd127a166fd80cdcde342f00e
isha-pargaonkar-14 <pargaonkar.i@northeastern.edu>
2026-06-12 22:58:41 -0700

    updated LabRole

--- terraform/modules/lambda-sast/main.tf
+++ terraform/modules/lambda-sast/main.tf
@@ -1,65 +1,17 @@
+data "aws_iam_role" "lab_role" {
+  name = "LabRole"
+}
+
 data "archive_file" "sast_lambda" {
   type        = "zip"
   source_dir  = "${path.root}/../../sast-lambda"
   output_path = "${path.module}/sast-lambda.zip"
 }
 
-resource "aws_iam_role" "sast_lambda" {
-  name = "securescan-sast-lambda-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = { Service = "lambda.amazonaws.com" }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy" "sast_lambda" {
-  name = "securescan-sast-lambda-policy"
-  role = aws_iam_role.sast_lambda.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
-        Resource = "arn:aws:logs:*:*:*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "dynamodb:UpdateItem", "dynamodb:GetItem", "dynamodb:PutItem"
-        ]
-        Resource = "arn:aws:dynamodb:*:*:table/${var.dynamodb_table}*"
-      },
-      {
-        Effect   = "Allow"
-        Action   = ["s3:PutObject", "s3:GetObject"]
-        Resource = "arn:aws:s3:::${var.s3_reports_bucket}/*"
-      },
-      {
-        Effect   = "Allow"
-        Action   = ["sns:Publish"]
-        Resource = var.sns_topic_arn
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "sqs:ReceiveMessage", "sqs:DeleteMessage",
-          "sqs:GetQueueAttributes"
-        ]
-        Resource = var.sast_queue_arn
-      }
-    ]
-  })
-}
-
 resource "aws_lambda_function" "sast" {
   filename         = data.archive_file.sast_lambda.output_path
   function_name    = "securescan-sast-scanner"
-  role             = aws_iam_role.sast_lambda.arn
+  role             = data.aws_iam_role.lab_role.arn
   handler          = "handler.handler"
   runtime          = "nodejs18.x"
   timeout          = 60
