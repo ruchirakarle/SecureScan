31c1823e218acb603eaf941eb1bceaee378396a4
isha-pargaonkar-14 <pargaonkar.i@northeastern.edu>
2026-06-12 22:28:12 -0700

    added ec2 and lambda-sast

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,35 @@ provider "aws" {
   region = var.aws_region
 }
 
+# ── EC2 Backend Module ─────────────────
+module "ec2" {
+  source = "./modules/ec2"
+
+  aws_region            = var.aws_region
+  vpc_id                = var.vpc_id
+  public_subnet_id      = var.public_subnet_id
+  ami_id                = var.ami_id
+  key_pair_name         = var.key_pair_name
+  dynamodb_table        = var.dynamodb_table
+  s3_reports_bucket     = var.s3_bucket
+  sqs_sast_queue_url    = aws_sqs_queue.scan_queue.url
+  sqs_pentest_queue_url = aws_sqs_queue.pentest_queue.url
+  sns_topic_arn         = aws_sns_topic.high_severity_alerts.arn
+  sast_scanner_url      = var.sast_scanner_url
+}
+
+# ── Lambda SAST Module ─────────────────
+module "lambda_sast" {
+  source = "./modules/lambda-sast"
+
+  aws_region        = var.aws_region
+  dynamodb_table    = var.dynamodb_table
+  s3_reports_bucket = var.s3_bucket
+  sns_topic_arn     = aws_sns_topic.high_severity_alerts.arn
+  sast_queue_arn    = aws_sqs_queue.scan_queue.arn
+  sast_scanner_url  = var.sast_scanner_url
+}
+
 # ── ECS Pentest Module─────────────────
 module "ecs_pentest" {
   source = "./modules/ecs-pentest"
