e3767d1b43214a16f8803835410dfaa2f27372a9
Ruchira Ravindra Karle <154701674+ruchirakarle@users.noreply.github.com>
2026-06-13 02:10:21 -0400

    Wire ECS and CloudWatch modules to real resource references

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,28 +40,27 @@ module "lambda_sast" {
   sast_scanner_url  = var.sast_scanner_url
 }
 
-# ── ECS Pentest Module─────────────────
+# ── ECS Pentest Module ─────────────────
 module "ecs_pentest" {
-  source = "./modules/ecs-pentest"
-
+  source             = "./modules/ecs-pentest"
   aws_region         = var.aws_region
-  vpc_id             = var.vpc_id
-  private_subnet_ids = var.private_subnet_ids
-
-  pentest_queue_url  = var.pentest_queue_url
-  pentest_queue_arn  = var.pentest_queue_arn
-  sns_topic_arn      = var.sns_topic_arn
-  s3_bucket          = var.s3_bucket
-  dynamodb_table     = var.dynamodb_table
-  dynamodb_table_arn = var.dynamodb_table_arn
+  vpc_id             = aws_vpc.securescan.id
+  private_subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+  pentest_queue_url  = aws_sqs_queue.pentest_queue.url
+  pentest_queue_arn  = aws_sqs_queue.pentest_queue.arn
+  sns_topic_arn      = aws_sns_topic.high_severity_alerts.arn
+  s3_bucket          = aws_s3_bucket.reports.bucket
+  dynamodb_table     = aws_dynamodb_table.scans.name
+  dynamodb_table_arn = aws_dynamodb_table.scans.arn
 }
+
 
 # ── CloudWatch Module ──────────────────
 module "cloudwatch" {
   source           = "./modules/cloudwatch"
-  sns_topic_arn    = var.sns_topic_arn
-  ec2_instance_id  = ""
+  sns_topic_arn    = aws_sns_topic.high_severity_alerts.arn
+  ec2_instance_id  = module.ec2.instance_id
   ecs_cluster_name = "securescan-cluster"
   ecs_service_name = "securescan-pentest-service"
-  dlq_name         = "securescan-scan-dlq-dev"
+  dlq_name         = aws_sqs_queue.scan_dlq.name
 }
