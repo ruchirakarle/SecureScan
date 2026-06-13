0f3903fd56a3dacd6338c228f5f7e9cc058d63c2
Ruchira Ravindra Karle <154701674+ruchirakarle@users.noreply.github.com>
2026-06-12 21:05:36 -0400

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
 
 resource "aws_vpc" "securescan" {
   cidr_block           = "10.0.0.0/16"
