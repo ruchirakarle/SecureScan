953482300a6f0ddd92e7f78f2b15f3b016fbf21d
Ruchira Ravindra Karle <154701674+ruchirakarle@users.noreply.github.com>
2026-06-12 21:05:11 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,37 +11,23 @@ provider "aws" {
   region = var.aws_region
 }
 
-# ── VPC Module─────────────────────────
-module "vpc" {
-  source = "./modules/vpc"
-
-  aws_region = var.aws_region
-}
-
-# ── ECS Pentest Module ─────────────────
+# ── ECS Pentest Module─────────────────
 module "ecs_pentest" {
   source = "./modules/ecs-pentest"
 
   aws_region         = var.aws_region
-  vpc_id             = module.vpc.vpc_id
-  private_subnet_ids = [
-    module.vpc.private_subnet_1_id,
-    module.vpc.private_subnet_2_id
-  ]
+  vpc_id             = var.vpc_id
+  private_subnet_ids = var.private_subnet_ids
 
-  # These come from Person 1's SNS/SQS terraform
-  # Leave as empty string until Person 1 deploys
   pentest_queue_url  = var.pentest_queue_url
   pentest_queue_arn  = var.pentest_queue_arn
   sns_topic_arn      = var.sns_topic_arn
-
-  # These come from Person 1's DynamoDB/S3 terraform
   s3_bucket          = var.s3_bucket
   dynamodb_table     = var.dynamodb_table
   dynamodb_table_arn = var.dynamodb_table_arn
 }
 
-# ── CloudWatch Module──────────────────
+# ── CloudWatch Module ──────────────────
 module "cloudwatch" {
   source           = "./modules/cloudwatch"
   sns_topic_arn    = var.sns_topic_arn
