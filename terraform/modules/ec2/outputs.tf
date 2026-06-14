a3a219cfe2c0f3fd1d9f08dc2cc9ffb62412a6eb
isha-pargaonkar-14 <pargaonkar.i@northeastern.edu>
2026-06-14 00:35:07 -0700

    Update outputs.tf

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -1,4 +1,4 @@
 output "instance_id"       { value = aws_instance.backend.id }
-output "public_ip"         { value = aws_instance.backend.public_ip }
-output "public_dns"        { value = aws_instance.backend.public_dns }
+output "public_ip"         { value = aws_eip.backend.public_ip }
+output "public_dns"        { value = aws_eip.backend.public_dns }
 output "security_group_id" { value = aws_security_group.backend.id }
